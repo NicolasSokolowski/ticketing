691863fdc9f018c9c74728cc9d84fa82f347f23c
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-06-28 20:21:53 +0200

    updated hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default ({ req }) => {
   if (typeof window === "undefined") {
-    // Server side
+    // Server side !
     return axios.create({
       baseURL: "http://www.ticketing-app-test.xyz/",
       headers: req.headers
     })
   } else {
-    // Browser side
+    // Browser side !
 
     return axios.create({
       baseUrl: "/"
